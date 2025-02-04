5f0e0524f0f5cbd0a49db0f743031a78a440d018
YannPl <yann.plantevin@meero.com>
2025-02-04 10:13:58 +0100

    ci(tests): attempt to use mysql in the CI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace Tests;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase {
-//    use RefreshDatabase;
+abstract class TestCase extends BaseTestCase
+{
+    use RefreshDatabase;
 }
