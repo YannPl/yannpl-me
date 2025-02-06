52818596ab256cdbfb237bcc9a39d5a36c7cac26
YannPl <yann.plantevin@meero.com>
2025-02-06 18:38:34 +0100

    feat(home): work on homepage template

---
+++ app/Livewire/Blog/Components/RelatedArticles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire\Blog\Components;
+
+use Livewire\Component;
+
+/**
+ * Display the 4 latest articles from the same category / tag
+ * If there are not enough articles, display the latest articles not already displayed
+ */
+class RelatedArticles extends Component {}
---
+++ resources/views/livewire/blog/components/article-excerpt.blade.php
@@ -0,0 +1,48 @@
+<article
+    class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+    <header class="mb-4 lg:mb-6 not-format">
+        <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+            Best practices for successful prototypes</h1>
+        <address class="flex text-sm items-center mb-6 not-italic">
+            <div class="inline-flex items-center mr-3 text-gray-900 dark:text-white">
+                <div>
+                    <a href="#" rel="author" class="font-bold text-gray-900 dark:text-white">John Doe</a>
+                    <div class="flex">
+                        <p class="text-gray-500 dark:text-gray-400">May 21, 2021</p>
+                        <span class="mx-2">|</span>
+                        <a href="#"
+                           class="bg-gray-100 hover:bg-gray-200 text-gray-800 text-xs font-semibold me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-gray-400 border border-gray-400 inline-flex items-center justify-center">Design</a>
+                        <span class="mx-2">|</span>
+                        <span
+                            class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">Tech</span>
+                        <span
+                            class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">Git</span>
+                        <span
+                            class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">PHP</span>
+                    </div>
+                </div>
+            </div>
+        </address>
+    </header>
+    <figure><img src="https://flowbite.s3.amazonaws.com/typography-plugin/typography-image-1.png" alt="main image">
+        <figcaption>Main image caption</figcaption>
+    </figure>
+
+    <p class="lead">Flowbite is an open-source library of UI components built with the utility-first
+        classes from Tailwind CSS. It also includes interactive elements such as dropdowns, modals,
+        datepickers.</p>
+    <p>Before going digital, you might benefit from scribbling down some ideas in a sketchbook. This way,
+        you can think things through before committing to an actual design project.</p>
+    <p>But then I found a <a href="https://flowbite.com">component library based on Tailwind CSS called
+            Flowbite</a>. It comes with the most commonly used UI components, such as buttons, navigation
+        bars, cards, form elements, and more which are conveniently built with the utility classes from
+        Tailwind CSS.</p>
+
+    <a href="#" class="inline-flex items-center no-underline font-medium text-blue-600 dark:text-blue-500 hover:underline">
+            Read more
+            <svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+
+</article>
