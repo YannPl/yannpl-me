7bb15a5da1fca9daf1c2705174fb6f6f9ac8e9b3
Yann Pl <yann.pltv@gmail.com>
2025-02-15 18:21:44 +0100

    feat(home): load articles dynamically on the home page

--- app/Livewire/Blog/Components/ArticleExcerpt.php
+++ app/Livewire/Blog/Components/ArticleExcerpt.php
@@ -2,6 +2,20 @@
 
 namespace App\Livewire\Blog\Components;
 
+use App\Models\Article;
+use App\Models\Category;
 use Livewire\Component;
 
-class ArticleExcerpt extends Component {}
+class ArticleExcerpt extends Component {
+    public Article $article;
+    public Category $category;
+    public string $isoDate;
+    public string $formattedDate;
+
+    public function mount(Article $article) {
+        $this->article = $article;
+        $this->category = $article->category;
+        $this->isoDate = $article->created_at->toIso8601String();
+        $this->formattedDate = $article->created_at->format('F j, Y');
+    }
+}
